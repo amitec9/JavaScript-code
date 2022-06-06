dfd69dcc94cb18740623293b5789d889d2433c75
Amit kumar <49529841+amitec9@users.noreply.github.com>
2022-06-06 23:42:23 +0530

    Update interviewQuestion25.js

--- interviewQuestion25.js
+++ interviewQuestion25.js
@@ -1,1 +1,21 @@
 
+function wordsReverser(){
+    let str3= "team tech  to welcome"
+    let words = str3.split(' ');
+    let data =[]
+    console.log(words)
+    for(i=words.length-1;i>=0;i--)
+    {
+        data.push(words[i])
+    
+    }
+    console.log(data)
+    // let result = [];
+    // for(let i = 0; i < words.length; i ++){
+    //   result.push(words[i].split('').reverse().join(''));
+    // }  
+    
+    // console.log(result.join(' '));
+ }
+ 
+wordsReverser()

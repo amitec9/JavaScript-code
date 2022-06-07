800fbcdc072b323f86908c7addd168475b60ed36
Amit kumar <49529841+amitec9@users.noreply.github.com>
2022-06-07 09:30:34 +0530

    Update interviewQuestion27.js

--- interviewQuestion27.js
+++ interviewQuestion27.js
@@ -1,1 +1,16 @@
 
+let arr =[1,2,3,10,5,1,0]
+
+for(let i =0;i<arr.length-1;i++)
+{
+    let temp;
+    if(arr[i]>arr[i+1])
+    {
+        temp = arr[i];
+        arr[i]= arr[i+1]
+        arr[i+1]=temp;
+        i = -1
+    }
+
+}
+console.log(arr)

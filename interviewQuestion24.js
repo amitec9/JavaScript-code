73801e805903e881ff84f686e233fa5dd92c4dbd
Amit kumar <49529841+amitec9@users.noreply.github.com>
2022-06-12 13:07:36 +0530

    Update interviewQuestion24.js

--- interviewQuestion24.js
+++ interviewQuestion24.js
@@ -1,1 +1,13 @@
 
+let obj = {name:"amit",last:"kumar"}
+let obj2 = {name:"rohan",last:"kumar"}
+
+let fullName = function(age , mob){
+console.log(this.name + " " + this.last , " age " + age , "mob " + mob )
+}
+//function bolwing
+
+fullName.call(obj,"25","999977900")
+fullName.apply(obj2,["27","900-09880"])
+let data = fullName.bind(obj2,"27","9097880")
+data()

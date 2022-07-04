4bd55554b0cc5b11cc9b021f9896ab60abbdc406
Amit kumar <49529841+amitec9@users.noreply.github.com>
2022-07-04 11:29:08 +0530

    Create interviewQuestion38.js

---
+++ interviewQuestion38.js
@@ -0,0 +1,37 @@
+if(false == ""){
+    console.log("1")
+}
+if(false == [])
+{
+    console.log("2")
+
+}
+if(false == {} ){
+    console.log("3")
+} 
+if("" == 0) 
+{
+    console.log("4")
+}   
+if("" == []){
+    console.log("5")
+}    
+if("" == {})
+{
+    console.log("6") 
+}    
+if(0 == [])
+{
+    console.log("7") 
+
+}      
+if(0 == {})
+{
+    console.log("8") 
+}      
+if(0 == null)   
+{
+    console.log("9") 
+
+
+}
